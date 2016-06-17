40f9693ea2c852349602ab225a054c3b04bf12a2
dima levchenko <ldimat@gmail.com>
2016-06-17 11:31:14 +0300

    inverse searching

---
+++ inverse/README.md
@@ -0,0 +1,9 @@
+Solution taken from [this comments](https://apps.topcoder.com/forums/?module=Thread&threadID=680416&mc=26&view=threaded#1262672).
+
+Results for 1.000.000.007:
+
+Measurement | Value
+------------|------
+MAX | 50
+AVG | 21
+
--- inverse/main.cpp
+++ inverse/main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
+#include <limits>
 #include <set>
 
 std::set<long> testing;
 
+long depth;
+long maxDepth = 0L;
+long maxDepthAll = 0L;
+long long avgDepth = 0LL;
+long long avgDepthAll = 0LL;
+
 long inv(long x, long P) {
+  ++depth;
   if(x == 1) return 1;
   if(testing.find(x) != testing.end()) {
-    std::cout << "Error while counting " << x << std::endl;
+    std::cerr << "Error while counting " << x << std::endl;
     return 0;
   }
   testing.insert(x);
@@ -15,11 +23,33 @@ long inv(long x, long P) {
   return P - ((P / x) * i) % P;
 }
 
+inline long max(long a, long b) { return a > b ? a : b; }
+
 int main() {
   long P = 1000000007L;
-  long x = 1000000006L;
-  long invX = inv(x, P);
+  for(long x = 2L; x < P - 1L; ++x) {
+    if(x % 100000L == 0) {
+      std::cout << "counting " << (x / 100000L) << ":"
+                << " max=" << maxDepth
+                << " avg=" << (avgDepth / 100000LL)
+                << std::endl;
+      maxDepthAll = max(maxDepthAll, maxDepth);
+      maxDepth = 0L;
+      avgDepthAll += avgDepth;
+      avgDepth = 0LL;
+    }
+    depth = 0L;
+    long invX = inv(x, P);
+    maxDepth = max(maxDepth, depth);
+    avgDepth += depth;
 
-  std::cout << invX << " " << (x * invX) % P << std::endl;
+    if(1 != (x * invX) % P) {
+      std::cerr << "Failed counting for " << x;
+    }
+  }
+  maxDepthAll = max(maxDepthAll, maxDepth);
+  avgDepthAll += avgDepth;
+  std::cout << "MAX: " << maxDepthAll << std::endl;
+  std::cout << "AVG: " << (avgDepthAll / 1000000006LL) << std::endl;
   return 0;
 }
