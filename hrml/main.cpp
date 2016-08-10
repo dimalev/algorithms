35c67ac4315ecbd5120c02c7b2da1b2b3660eb84
dima levchenko <ldimat@gmail.com>
2016-08-10 16:00:28 +0300

    HRML solution

--- hrml/main.cpp
+++ hrml/main.cpp
@@ -15,30 +15,34 @@ public:
 
 class Node {
 public:
-  // Node(): parent{nullptr} {}
-  Tag open, close;
-  std::vector<Node> children;
+  Tag *open, *close;
+  std::vector<Node*> children;
   Node* parent = nullptr;
 };
 
 class Pointer {
 public:
-  std::string from(Node node) {
-    Node *head = &node;
-    for(std::string tag : tags) {
+  std::string from(Node *node) {
+    Node *head = node;
+    int tl = tags.size();
+    for(int i = 0; i < tl; ++i) {
+      std::string tag = tags[i];
+#ifdef ALGO_DEBUG
+      std::cerr << "seek tag: " << tag << std::endl;
+#endif
       Node *match = nullptr;
-      for(Node& child : head->children) {
-        if(child.open.name == tag) {
-          match = &child;
+      for(Node* child : head->children) {
+        if(child->open->name == tag) {
+          match = child;
           break;
         }
       }
-      if(match == nullptr) return "Not Found";
+      if(match == nullptr) return "Not Found!";
       head = match;
     }
-    auto attrit = head->open.attrs.find(attr);
-    if(attrit != head->open.attrs.end()) return attrit->second;
-    return "Not Found";
+    auto attrit = head->open->attrs.find(attr);
+    if(attrit != head->open->attrs.end()) return attrit->second;
+    return "Not Found!";
   }
 
   std::vector<std::string> tags;
@@ -47,82 +51,119 @@ public:
 
 Pointer operator>>(const std::istream& in, Pointer &result) {
   std::string line;
-  std::cin >> line;
+  std::getline(std::cin, line);
+#ifdef ALGO_DEBUG
+  std::cerr << "got pointer: " << line << std::endl;
+#endif
   std::size_t last_period_pos = 0;
   while(true) {
     std::size_t period_pos = line.find('.', last_period_pos);
     if(period_pos == std::string::npos) break;
-    result.tags.push_back(line.substr(last_period_pos + 1, period_pos));
-    last_period_pos = period_pos;
+    std::string tagName = line.substr(last_period_pos, period_pos - last_period_pos);
+#ifdef ALGO_DEBUG
+    std::cerr << "new tag name: " << tagName << std::endl;
+#endif
+    result.tags.push_back(tagName);
+    last_period_pos = period_pos + 1;
   }
   std::size_t tyld_pos = line.find('~', last_period_pos);
-  result.tags.push_back(line.substr(last_period_pos + 1, tyld_pos));
+  std::string lastTag = line.substr(last_period_pos, tyld_pos - last_period_pos);
+#ifdef ALGO_DEBUG
+    std::cerr << "last tag name: " << lastTag << std::endl;
+#endif
+  result.tags.push_back(lastTag);
   result.attr = line.substr(tyld_pos + 1);
+#ifdef ALGO_DEBUG
+    std::cerr << "attribute: " << result.attr << std::endl;
+#endif
   return result;
 }
 
-void skipSpaces(const std::istream& in) {
+void skipSpaces(std::istream& in) {
   char c;
   while(!in.eof()) {
     in.get(c);
     if(!std::isspace(c)) {
       in.putback(c);
+      return;
     }
+#ifdef ALGO_DEBUG
+    std::cerr << "- skip char: " << c << std::endl;
+#endif
   }
 }
 
-Tag operator>>(const std::istream& in, Tag &tag) {
+bool readTag(std::istream &in, Tag *tag) {
   skipSpaces(in);
   char mandatory;
-  in >> mandatory;
+  in.get(mandatory);
 #ifdef ALGO_DEBUG
   std::cerr << "open tag: " << mandatory << "." << std::endl;
+  if(mandatory != '<') return false;
 #endif
   char isclose;
-  in >> isclose;
+  in.get(isclose);
 #ifdef ALGO_DEBUG
   std::cerr << "is close: " << isclose << "." << std::endl;
 #endif
   if(isclose == '/') {
-    tag.type = Tag::Type::close;
+    tag->type = Tag::Type::close;
   } else {
-    tag.type = Tag::Type::open;
+    tag->type = Tag::Type::open;
     in.putback(isclose);
   }
-  in >> tag.name;
+  char tagC;
+  in.get(tagC);
+  while(tagC != '>' && !std::isspace(tagC)) {
+    tag->name += tagC;
+    in.get(tagC);
+  }
+  if(tagC == '>') in.putback(tagC);
 #ifdef ALGO_DEBUG
-  std::cerr << "tag name: " << tag.name << "." << std::endl;
+  std::cerr << "tag name: " << tag->name << "." << std::endl;
 #endif
   skipSpaces(in);
-  if(tag.type == Tag::Type::open) {
+  if(tag->type == Tag::Type::open) {
     while(true) {
       skipSpaces(in);
-      in >> mandatory;
+      in.get(mandatory);
 #ifdef ALGO_DEBUG
       std::cerr << "check if tag end: " << mandatory << "." << std::endl;
 #endif
       if(mandatory == '>') break;
       else in.putback(mandatory);
       std::string attrName, attrValue;
-      in >> attrName;
+      char c;
+      in.get(c);
+      while(c != '=' && !std::isspace(c)) {
+        attrName += c;
+        in.get(c);
+      }
 #ifdef ALGO_DEBUG
       std::cerr << "got attribute name: " << attrName << "." << std::endl;
 #endif
       skipSpaces(in);
-      in >> mandatory;
+      if(c != '=') {
+        skipSpaces(in);
+        in.get(mandatory);
+      } else mandatory = c;
 #ifdef ALGO_DEBUG
-      std::cerr << "try equal: " << mandatory << "." << std::endl;
+      std::cerr << "mandatory equal: " << mandatory << "." << std::endl;
 #endif
       skipSpaces(in);
-      in >> attrValue;
+      in.get(mandatory);
 #ifdef ALGO_DEBUG
-      std::cerr << "got value before stripping: " << attrValue << "." << std::endl;
+      std::cerr << "mandatory double quote: " << mandatory << "." << std::endl;
 #endif
-      attrValue = attrValue.substr(1, attrValue.length() - 2);
+      in.get(c);
+      while(c != '"' && !std::isspace(c)) {
+        attrValue += c;
+        in.get(c);
+      }
 #ifdef ALGO_DEBUG
-      std::cerr << "value after stripping: " << attrValue << "." << std::endl;
+      std::cerr << "value: " << attrValue << "." << std::endl;
 #endif
-      tag.attrs[attrName] = attrValue;
+      tag->attrs[attrName] = attrValue;
     }
   } else {
     in >> mandatory;
@@ -130,41 +171,61 @@ Tag operator>>(const std::istream& in, Tag &tag) {
     std::cerr << "should close tag: " << mandatory << "." << std::endl;
 #endif
   }
-  return tag;
+  return true;
 }
 
-Node operator>>(const std::istream& in, Node &data) {
-  Node *current = &data;
-  in >> current->open;
+bool readHrml(std::istream& in, Node *data) {
+  Node *current = data;
+#ifdef ALGO_DEBUG
+  std::cerr << "before reading tag" << std::endl;
+#endif
+  current->open = new Tag{};
+  if(!readTag(in, current->open)) return false;
   while(current != nullptr) {
-    Tag nextTag;
-    in >> nextTag;
-    if(nextTag.type == Tag::Type::close) {
+    Tag *nextTag = new Tag{};
+    readTag(in, nextTag);
+    if(nextTag->type == Tag::Type::close) {
       current->close = nextTag;
       current = current->parent;
     } else {
       Node* nextNode = new Node{};
-      current.children.push_back(nextNode);
+      current->children.push_back(nextNode);
       nextNode->open = nextTag;
+      nextNode->parent = current;
       current = nextNode;
     }
   }
+  return true;
 }
 
 int main() {
   int n, m;
   std::cin >> n >> m;
   std::stringstream hrml;
+  std::string line;
+  std::getline(std::cin, line);
   for(int i = 0; i < n; ++i) {
-    std::string line;
-    std::cin >> line;
+    std::getline(std::cin, line);
     hrml << line;
   }
-  Node data;
-  hrml >> data;
+#ifdef ALGO_DEBUG
+  std::cerr << "read done" << std::endl;
+#endif
+  Node *data = new Node{};
+  while(hrml.rdbuf()->in_avail()) {
+    Node *nextChild = new Node{};
+    if(!readHrml(hrml, nextChild)) break;;
+    data->children.push_back(nextChild);
+  }
+#ifdef ALGO_DEBUG
+  std::cerr << "parse done" << std::endl;
+#endif
   for(int i = 0; i < m; ++i) {
     Pointer p;
     std::cin >> p;
+#ifdef ALGO_DEBUG
+    std::cerr << "next pointer" << std::endl;
+#endif
     std::cout << p.from(data) << std::endl;
   }
   return 0;
