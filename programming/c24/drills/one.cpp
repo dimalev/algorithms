de3ac9623bc8f25eb3c99b03748228dd5d1b70f8
dmytro levchenko <ldimat@gmail.com>
2016-10-15 09:53:40 +0300

    more type sizes

--- programming/c24/drills/one.cpp
+++ programming/c24/drills/one.cpp
@@ -2,21 +2,28 @@
 
 #include <iostream>
 #include <typeinfo>
+#include <string>
 
 template<typename T>
-void printSize() {
-  std::cout << "type " << typeid(T).name() << " size = " << sizeof(T) << std::endl;
+void printSize(const std::string msg = "") {
+  if(!msg.empty()) std::cout << "type " << msg << "(" << typeid(T).name() << ")";
+  else std::cout << "type " << typeid(T).name();
+  std::cout << " size = " << sizeof(T) << std::endl;
 }
 
 int main() {
   printSize<char>();
   printSize<short>();
   printSize<int>();
+  printSize<unsigned int>("unsigned int");
   printSize<long>();
+  printSize<unsigned long>("unsigned long");
   printSize<float>();
   printSize<double>();
   printSize<long double>();
   printSize<int*>();
   printSize<double*>();
+  printSize<size_t>("size_t");
+  printSize<ptrdiff_t>("ptrdiff_t");
   return 0;
 }
