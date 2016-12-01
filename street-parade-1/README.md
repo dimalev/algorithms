ac57d0b785eadc96111000abc14f0e915693f283
dima levchenko <ldimat@gmail.com>
2016-12-01 09:42:25 +0200

    street parade empty project

---
+++ street-parade-1/README.md
@@ -0,0 +1,6 @@
+Problem
+-------
+
+[Music on the Street][https://www.hackerrank.com/contests/w26/challenges/street-parade-1] from
+hackerrank.
+
---
+++ street-parade-1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#ifdef ALGO_DEBUG
+#include "../test/debug.cpp"
+#else
+
+#define TRACE(message)
+#define TRACE_LINE(message)
+#define ASSERT(expr)
+#define UNIT_TESTS()
+
+#endif
+
+void unit_tests() {
+}
+
+int main() {
+  UNIT_TESTS();
+  return 0;
+}
