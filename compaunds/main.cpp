fc90396f12894945b071a82d4e66b63312534ff0
dmytro levchenko <dmytro.levchenko@ring.com>
2017-07-26 21:14:54 +0300

    implement compaunds

---
+++ compaunds/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+#define fr(v,s,e) for(int v = s; v < e; ++v)
+#define fl(v,s,e) for(int v = s; v > e; --v)
+
+#ifdef UNITS
+#include "../test/units.cpp"
+#endif
+
+#ifdef ALGO_DEBUG
+#include "../test/debug.cpp"
+#else
+#define TRACE(message)
+#define TRACE_LINE(message)
+#define ASSERT(expr)
+#endif
+
+#ifdef UNITS
+void unit_tests() {
+  test_header("test units");
+}
+#endif
+
+int main() {
+#ifdef UNITS
+  unit_tests();
+  return 0;
+#endif
+  int V, N;
+  
+  return 0;
+}
