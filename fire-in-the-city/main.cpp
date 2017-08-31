d51b4ace784686565ad1b59770a847d615bfd7f6
dmytro levchenko <dmytro@agile.in.ua>
2017-08-31 07:41:59 +0300

    fix fire in the city

--- fire-in-the-city/main.cpp
+++ fire-in-the-city/main.cpp
@@ -4,8 +4,8 @@
 #include <set>
 #include <map>
 
-#define fr(v,s,e) for(int v = s; v < e; ++v)
-#define fl(v,s,e) for(int v = s; v > e; --v)
+#define fr(v,s,e) for(long long v = s; v < e; ++v)
+#define fl(v,s,e) for(long long v = s; v > e; --v)
 
 #ifdef UNITS
 #include "../test/units.cpp"
@@ -30,32 +30,32 @@ int main() {
   unit_tests();
   return 0;
 #endif
-  long rows, cols, fires_count;
+  long long rows, cols, fires_count;
   std::cin >> rows >> cols >> fires_count;
-  std::vector<std::pair<int, int>> fires;
+  std::vector<std::pair<long long, long long>> fires;
   fires.resize(fires_count);
   fr(i,0,fires_count) {
-    int r, c;
+    long long r, c;
     std::cin >> r >> c;
     fires[i].first = r - 1;
     fires[i].second = c - 1;
   }
-  long l = -1, r = std::max(rows, cols);
+  long long l = -1, r = std::max(rows, cols);
   while(l + 1 < r) {
-    long m = (l + r) / 2;
-    std::set<int> R, C;
+    long long m = (l + r) / 2;
+    std::set<long long> R, C;
     R.insert(0); R.insert(rows);
     C.insert(0); C.insert(cols);
     fr(i,0,fires_count) {
-      R.insert(std::max(0l, fires[i].first - m));
+      R.insert(std::max(0ll, fires[i].first - m));
       R.insert(std::min(rows, fires[i].first + m + 1));
-      C.insert(std::max(0l, fires[i].second - m));
+      C.insert(std::max(0ll, fires[i].second - m));
       C.insert(std::min(cols, fires[i].second + m + 1));
     }
     TRACE_LINE("m = " << m);
-    std::map<int, int> Ri, Ci;
-    int Rv[R.size()], Cv[C.size()];
-    int it = 0;
+    std::map<long long, long long> Ri, Ci;
+    long long Rv[R.size()], Cv[C.size()];
+    long long it = 0;
     for(auto v : R) {
       Rv[it] = v;
       Ri[v] = it;
@@ -72,15 +72,15 @@ int main() {
     bool on_fire[R.size()][C.size()];
     std::fill_n((bool*)on_fire, R.size() * C.size(), false);
     fr(i,0,fires_count) {
-      int start_r = Ri[std::max(0l, fires[i].first - m)];
-      int end_r = Ri[std::min(rows, fires[i].first + m + 1)];
-      int start_c = Ci[std::max(0l, fires[i].second - m)];
-      int end_c = Ci[std::min(cols, fires[i].second + m + 1)];
+      long long start_r = Ri[std::max(0ll, fires[i].first - m)];
+      long long end_r = Ri[std::min(rows, fires[i].first + m + 1)];
+      long long start_c = Ci[std::max(0ll, fires[i].second - m)];
+      long long end_c = Ci[std::min(cols, fires[i].second + m + 1)];
       fr(r,start_r,end_r)
         fr(c,start_c,end_c)
           on_fire[r][c] = true;
     }
-    int min_r = rows, max_r = 0, min_c = cols, max_c = 0;
+    long long min_r = rows, max_r = 0, min_c = cols, max_c = 0;
     fr(r,0,R.size() - 1) {
       TRACE(Rv[r]);
       fr(c,0,C.size() - 1) {
@@ -95,11 +95,13 @@ int main() {
       TRACE_LINE("");
     }
     TRACE_LINE(Rv[R.size() - 1]);
+    TRACE_LINE(min_r << " - " << max_r << ", " << min_c << " - " << max_c);
     if(min_r > max_r) {
       r = m;
     } else {
-      long dr = Rv[max_r + 1] - Rv[min_r], dc = Cv[max_r + 1] - Cv[min_r];
-      long max_d = std::max(dr, dc) / 2;
+      long long dr = Rv[max_r + 1] - Rv[min_r], dc = Cv[max_c + 1] - Cv[min_c];
+      TRACE_LINE(dr << ", " << dc);
+      long long max_d = std::max(dr, dc) / 2;
       if(max_d <= m) r = m;
       else l = m;
     }
