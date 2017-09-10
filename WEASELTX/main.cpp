1ea4798a62990155a34ff678fcce5aebf6c98a3d
dmytro levchenko <dmytro@agile.in.ua>
2017-09-11 02:15:39 +0300

    code chef septempber challenge

--- WEASELTX/main.cpp
+++ WEASELTX/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <iomanip>
 #include <algorithm>
+#include <queue>
+#include <map>
+#include <set>
 
 #define fr(v,s,e) for(int v = s; v < e; ++v)
 #define fl(v,s,e) for(int v = s; v > e; --v)
@@ -16,25 +20,7 @@
 #define ASSERT(expr)
 #endif
 
-constexpr long long max_m = 0b1100000000000000000000000000000000000000000000000000000000000;
-constexpr long long max_d = 59;
-
-int check(long long a, long long b) {
-  long long M = max_m;
-  long long D = max_d;
-  long long x, y;
-  while(D > 0) {
-    x = (a & M) >> D;
-    y = (b & M) >> D;
-    if(y > x) return 0;
-    // std::cout << M << ", " << D << ": "
-    //           << x << " vs " << y << std::endl;
-    --D; M >>= 1;
-  }
-  x = (a & M) >> D;
-  y = (b & M) >> D;
-  return (y > x || (x == 2 && y == 1)) ? 0 : 1;
-}
+constexpr long long base = 0b10000000000000000000000000000000000000000000000000000000000000 - 1;
 
 #ifdef UNITS
 void unit_tests() {
@@ -47,20 +33,80 @@ int main() {
   unit_tests();
   return 0;
 #endif
-  bool M[100];
-  M[0] = 1;
-  std::fill_n(M + 1, 99, 0);
-  // check(0, 1);
-  fr(i,0,150) {
-    fr(j,0,150) {
-      // std::cout << M[j];
-      std::cout << check(i, j);
-    }
-    std::cout << std::endl;
-    // fl(j,99,0) {
-    //   M[j] = M[j] xor M[j - 1];
-    // }
+  int N, Q;
+  std::cin >> N >> Q;
+  std::map<int, std::set<int>> E;
+  fr(i,0,N-1) {
+    int u, v;
+    std::cin >> u >> v;
+    E[u].insert(v);
+    E[v].insert(u);
   }
-  std::cout << std::endl;
+  std::queue<int> next;
+  int D[N];
+  std::fill_n(D, N, -1);
+  D[0] = 0;
+  next.push(0);
+  while(!next.empty()) {
+    int r = next.front();
+    next.pop();
+    for(const auto &v : E[r]) {
+      if(D[v] == -1) {
+        D[v] = D[r] + 1;
+        next.push(v);
+      }
+    }
+  }
+  long long max_depth = (long long)*std::max_element(D, D + N);
+  long long V[N];
+  std::fill_n(V, max_depth + 1, 0ll);
+  fr(i,0,N) {
+    long long v;
+    std::cin >> v;
+    V[D[i]] = V[D[i]] xor v;
+  }
+  long long mask = 1ll;
+  while(mask <= max_depth) mask <<= 1;
+  long long cache[mask];
+  mask -= 1;
+  if(mask < 128) {
+    for(long long i = 0; i <= mask; ++i) {
+      long long res = 0ll;
+      for(long long j = 0; j <= std::min(i, max_depth); ++j) {
+        if(((i xor j) & j) == 0) res = res xor V[j];
+      }
+      cache[i] = res;
+    }
+  } else {
+    long long orig_mask = mask;
+    mask >>= 6;
+    long long d = mask + 1;
+    for(long long i = 0; i <= mask; ++i) {
+      long long res[64];
+      std::fill_n(res, 64, 0ll);
+      for(long long j = 0; j <= std::min(i, max_depth); ++j) {
+        if(((i xor j) & j) == 0) {
+          fr(k,0,64) {
+            if(k * d + j <= max_depth) res[k] = res[k] xor V[k * d + j];
+          }
+        }
+      }
+      for(long long k = 0; k < 64; ++k) {
+        long long out = 0ll;
+        for(long long j = 0; j < 64; ++j) {
+          if(((k xor j) & j) == 0) out = out xor res[j];
+        }
+        cache[k * d + i] = out;
+      }
+    }
+    mask = orig_mask;
+  }
+  fr(i,0,Q) {
+    long long d;
+    std::cin >> d;
+    d = base + 1ll - d;
+    d = d & mask;
+    std::cout << cache[d] << std::endl;
+  }
   return 0;
 }
